583f688c8764862faf65360ed1514af474cc5ee2
Sampson Orson Jackson <sampson.orson@gmail.com>
2022-03-03 19:44:51 +0100

    use dynamic import inside setTimeout

--- script.js
+++ script.js
@@ -4,7 +4,9 @@ import { default as User, printUser } from "./user.js";
 /* const user = new User("John", "Doe");
 printUser(user); */
 
-import('./user.js').then(({ default: User, printUser }) => {
-    const user = new User("John", "Doe");
-    printUser(user);
-});
+setTimeout(() => {
+    import('./user.js').then(({ default: User, printUser }) => {
+        const user = new User("John", "Doe");
+        printUser(user);
+    });
+}, 5000);
